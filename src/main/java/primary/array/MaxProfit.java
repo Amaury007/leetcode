2f8335092fd0eced80ef102ab6eeb930e7c3d26b
Amaury007 <pyhsemail@163.com>
2020-12-14 14:40:15 +0800

    leetcode

---
+++ src/main/java/primary/array/MaxProfit.java
@@ -0,0 +1,23 @@
+package primary.array;
+
+/**
+ * @Author sannian
+ * @Date 2020/12/14 11:29
+ * @Version 1.0
+ */
+
+public class MaxProfit {
+    public static void main(String[] args) {
+        int[] nums = {7,1,5,3,6,4};
+        int len = unique1(nums);
+    }
+
+    private static int unique1(int[] nums) {
+        int maxnum = 0;
+        if (nums.length == 0){
+            return maxnum;
+        }
+
+        return maxnum;
+    }
+}
